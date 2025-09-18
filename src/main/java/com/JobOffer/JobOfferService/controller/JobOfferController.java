7d1dc25d83f76a60642e62d10be87638c1a3d4f4
Barakat Adnane <adnanebarakat3@gmail.com>
2025-09-18 23:01:20 +0100

    Added services

--- src/main/java/com/JobOffer/JobOfferService/controller/JobOfferController.java
+++ src/main/java/com/JobOffer/JobOfferService/controller/JobOfferController.java
@@ -4,36 +4,44 @@ import com.JobOffer.JobOfferService.dto.JobOfferDto;
 import com.JobOffer.JobOfferService.dto.wrapper.PaginatedResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import com.JobOffer.JobOfferService.Service.JobOfferService;
 
 import java.util.UUID;
 
 @Controller
+@RequestMapping("/service/JobOffer")
 public class JobOfferController {
     private final JobOfferService jobservice;
     public JobOfferController(JobOfferService jobservice){
         this.jobservice=jobservice;
     }
 
-    @GetMapping("/offers/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<JobOfferDto> getOffer(@PathVariable UUID id) {
         return ResponseEntity.ok(jobservice.findOne(id));
     }
 
 
-    @GetMapping("/publish")
+    @PostMapping
     public JobOfferDto publishOffer(@RequestBody JobOfferDto newJob , String requestor){
             return jobservice.publishNewJobOffer( newJob , requestor);
     }
 
-    @GetMapping("/searchOffers")
+    @GetMapping
     public PaginatedResponseDto<JobOfferDto> search(@RequestBody String title , int page , int size){
         return jobservice.search(title , page , size);
     }
 
-    @GetMapping()
+    @PutMapping
+    public ResponseEntity<JobOfferDto> updateOffer(@RequestBody JobOfferDto updatedJob , String requestor){
+        return ResponseEntity.ok(jobservice.updateJobOffer(updatedJob,requestor));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOffer(@PathVariable UUID id, String requestor){
+        jobservice.cancelJobOffer(id,requestor);
+        return ResponseEntity.ok().build();
+    }
 
 }
